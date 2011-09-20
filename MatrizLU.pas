1221a01e5b1e5736669e992a137d62934746085e
André <andreguipeil@gmail.com>
2011-09-20 12:31:33 -0300

    Pascalzim Pronto bombando

--- MatrizLU.pas
+++ MatrizLU.pas
@@ -1,73 +1,88 @@
 // -------------------------------------------------------------
-// Trabalho Conceitos de Linguagens de Programação
+// Trabalho Conceito e Linguagens de Programacao
 // Integrantes: Andre Peil, Daniel Retzlaff, Marlon Dias
 // -------------------------------------------------------------
 
 Program MatrizLU;
 
- Var mComp, mLow, mUp : array [ 1..4 , 1..4 ] of integer;
-     i,j : integer;
+ Var mComp, mL, mU : array [ 1..4 , 1..4 ] of real;
+     temp : real;
+     i,j,k : integer;
 
  Begin
 
-  // Leitura da matriz completa
-  For i := 1 to 3 do
-    For j := 1 to 3 do
-     Begin
-       writeln('adsadasda[',i, ',',j, ']:');
-       readln(mComp[i, j]);
-     End;
+  writeln('Matriz A: ');
 
+  // Leitura da matriz A e inicializacao de matris LU
+  For i := 1 to 3 do
+    Begin
+		For j := 1 to 3 do
+
+		Begin
+			write('A [',i, ',',j, ']: ');
+			readln(mComp[i, j]);
+			mL[i, j] := 0;
+			mU[i, j] := 0;
+		End;
+    mL[i, i] := 1;
+	End;
 	
-  // Calcula e insere valores nas matrizes auxiliares
-  For i:= 1 to 3  do
-    For j:= 1 to 3 do
-     Begin
-  // Se i e j forem diferentes entao
-       if (i <> j) then 
-		Begin 
-  // se i for maior que j entao preenche a matriz low     
-          if (i > j) then
-             mLow[i, j] := mComp[i, j]
-  // senao preenche com 1 (parte de cima)
-           else
-             mLow[i, j] := 1;
-  // se i for menor que j entao preenche a matriz up          
-          if (i < j) then
-             mUp[i, j] := mComp[i, j]
-  // senao preenche com 1 (parte de baixo)
-          else
-             mUp[i, j] := 1;
-  // senao, se i e j forem igual recebe 0
-        end
-        else
-        Begin
-        mLow[i, j] := 0;
-        mUp[i, j] := 0;
-	   end;
-	end;
+	// Decomposicao LU
+
+	For i:= 1 to 3 do
+		Begin
+			For j:= 1 to 3 do
+				Begin
+				if (i <= j) then
+					Begin
+					temp := 0;
+					For k := 1 to i do
+						Begin
+						temp := temp + (mL[i, k]*mU[k, j]);
+						End;
+					mU[i, j] := mComp[i, j] - temp;	
+					End
+					Else
+						Begin
+							temp := 0;
+							For k := 1 to j do
+								Begin
+								temp := temp + (mL[i, k] * mU[k, j]);
+								End;
+							If (mU[j, j] = 0) then
+								Begin
+									writeln('Erro... Divisao por Zero');
+									exit;
+								End
+								Else
+									Begin
+										mL[i, j] := (mComp[i, j] - temp) / mU [j, j];
+									End;
+						End;
+					
+				
+				End;
+		End;
+
+
   // Impressao das matrizes
   writeln('Matriz Comp: ');
   For i:= 1 to 3 do
-     writeln(mComp[i,1]:4, mComp[i,2]:4, mComp[i,3]:4);
-  
-  writeln('Matriz Low: ');
-  
-  For i:= 1 to 3 do
-     writeln(mLow[i,1]:4, mLow[i,2]:4, mLow[i,3]:4);
-  writeln('Matriz Up: ');
-  
-  For i:= 1 to 3 do
-     writeln(mUp[i,1]:4, mUp[i,2]:4, mUp[i,3]:4);
-  
+     writeln(mComp[i,1]:2:1,'  ', mComp[i,2]:2:1,'  ', mComp[i,3]:2:1);
 
-  
-  
+  writeln('Matriz Low: ');
+
+  For i:= 1 to 3 do
+     writeln(mL[i,1]:2:1,'  ',mL[i,2]:2:1,'  ',mL[i,3]:2:1);
+  writeln('Matriz Up: ');
+
+  For i:= 1 to 3 do
+     writeln(mU[i,1]:2:1,'  ',mU[i,2]:2:1,'  ',mU[i,3]:2:1);
 
 
  End.
- 
- 
- 
- 
- 
+
+
+
+
+
